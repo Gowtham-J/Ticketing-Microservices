5f5747a89d486d44b8ece9e2f6afc03db4dc0103
Gowtham J <gowthamj@chimeratechnologies.com>
2022-01-11 17:53:32 +0530

    debug unsolved

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -3,6 +3,10 @@ import axios from "axios";
 const BuildClient = ({ req }) => {
   if (typeof window === "undefined") {
     // We are on the server
+    // baseURL: "http://ticketing-microservice-app.xyz",
+    // baseURL:
+    //     "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local",
+
     return axios.create({
       baseURL: "http://ticketing-microservice-app.xyz",
       headers: req.headers,
